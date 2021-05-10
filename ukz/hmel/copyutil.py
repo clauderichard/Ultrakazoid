c6cc36b552ec10d3354eb9f95f0d4873826ef007
Claude Richard <claude.richard2@gmail.com>
2021-05-10 10:21:08 -0400

    Delete ukz/hmel directory

--- ukz/hmel/copyutil.py
+++
@@ -1,9 +0,0 @@
-
-################################
-
-def trycopy(x):
-  return None if x is None else x.copy()
-def listcopies(xs):
-  return list(map(trycopy, xs))
-
-################################
